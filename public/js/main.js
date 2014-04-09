9093d64de5d819e6ee0fe90c3b4e5f367f8d1079
Myrousz <myrousz@gmail.com>
2014-04-09 15:18:06 +0200

    ...

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,48 @@
 $(document).ready(function() {
-	alert("Hello, jquery!")
+	
+	var eventsDescUri = "data/events.json"
+
+	var root = $("#eventdocs")
+
+	function displayClasses(cls) {
+		for(var i=0; i<cls.length; i++) {
+			displayClass(cls[i])
+		}
+	}
+
+	function displayClass(cls) {
+		// class either establishes new event class
+		// or uses existing one
+		if(cls.uses) {
+			displayUseClass(cls)
+		} else {
+			displayDefClass(cls)
+		}
+	}
+
+	function displayDefClass(cls) {
+		console.log("Display class definition", cls)
+		var out = $("<div>")
+			.addClass("eventclass def")
+			.append($("<h2>").text(cls.name))
+			.append($("<h3>").text(cls.className))
+			.append($("<h4>").text(cls['extends'] || ""))
+			.append($("<div>").text(cls.description))
+		root.append(out)
+	}
+
+	function displayUseClass(cls) {
+		console.log("Display derived class", cls)
+		var out = $("<div>")
+			.addClass("eventclass use")
+			.append($("<h2>").text(cls.name))
+			.append($("<h3>").text(cls.uses))
+			.append($("<div>").text(cls.description))
+		root.append(out)
+	}
+
+	$.getJSON(eventsDescUri).done(function(data) {
+		console.log(data)
+		displayClasses(data.classes)
+	})
 })
